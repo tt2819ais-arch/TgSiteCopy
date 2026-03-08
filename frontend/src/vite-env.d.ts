fdac41d88c87a9e416e7c1a7973d489e64f7e666
tt2819ais-arch <tt2819ais@gmail.com>
2026-03-08 20:36:06 +0300

    Create vite-env.d.ts

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="vite/client" />
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}

b6c7df912c01fc7a0800cd426842c81ecdb31d9f
tt2819ais-arch <tt2819ais@gmail.com>
2026-03-08 20:38:27 +0300

    Create index.ts

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,41 @@
+export type {
+  User,
+  UserProfile,
+  UserStatus,
+  UserRole,
+  PublicUser,
+} from './user';
+
+export type {
+  Message,
+  MessageType,
+  MessageStatus,
+  MessageReaction,
+  MessageAttachment,
+  AttachmentType,
+} from './message';
+
+export type {
+  Chat,
+  ChatType,
+  ChatParticipant,
+  ChatPreview,
+} from './chat';
+
+export type {
+  Gift,
+  GiftCategory,
+  GiftTransaction,
+  GiftRarity,
+  UserGift,
+} from './gift';
+
+export type {
+  ApiResponse,
+  PaginatedResponse,
+  WSMessage,
+  WSEventType,
+  Toast as ToastType,
+  ToastVariant,
+  ContextMenuItem,
+} from './common';
